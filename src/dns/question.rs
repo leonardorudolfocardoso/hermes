c66359b6d276190829ac49b19dcbb82120d2b150
Leonardo C. <leonardorudolfocardoso@gmail.com>
2026-06-10 18:32:43 -0300

    feat: question encode

--- src/dns/question.rs
+++ src/dns/question.rs
@@ -1,9 +1,13 @@
 use std::io::Result;
 
 use super::name::Name;
-use crate::{Decode, reader::PacketReader};
+use crate::{
+    Decode, Encode,
+    reader::PacketReader,
+    writer::{PacketWriter, WriteResult},
+};
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct Question {
     name: Name,
     record_type: u16,
@@ -21,5 +25,64 @@ impl Decode for Question {
             record_type,
             class,
         })
+    }
+}
+
+impl Encode for Question {
+    fn encode(&self, writer: &mut PacketWriter) -> WriteResult {
+        let mut n = 0;
+        n += self.name.encode(writer)?;
+        n += writer.write_u16(self.record_type)?;
+        n += writer.write_u16(self.class)?;
+        Ok(n)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{
+        Decode, Encode,
+        dns::{name::Name, question::Question},
+        reader::PacketReader,
+        writer::PacketWriter,
+    };
+
+    #[test]
+    fn question_encode_writes_correct_bytes() {
+        let question = Question {
+            name: Name::from("google.com"),
+            record_type: 1,
+            class: 1,
+        };
+
+        let mut writer = PacketWriter::new();
+
+        question.encode(&mut writer).unwrap();
+
+        assert_eq!(
+            writer.get(),
+            &[
+                6, b'g', b'o', b'o', b'g', b'l', b'e', 3, b'c', b'o', b'm', 0, 0x00, 0x01, 0x00,
+                0x01,
+            ]
+        );
+    }
+    #[test]
+    fn question_round_trip() {
+        let original = Question {
+            name: Name::from("google.com"),
+            record_type: 1,
+            class: 1,
+        };
+
+        let mut writer = PacketWriter::new();
+
+        original.encode(&mut writer).unwrap();
+
+        let mut reader = PacketReader::new(writer.get());
+
+        let decoded = Question::decode(&mut reader).unwrap();
+
+        assert_eq!(decoded, original);
     }
 }
