b75edfd2ac07446dc691ca821a93db3be0c7ff0e
Leonardo C. <leonardorudolfocardoso@gmail.com>
2026-06-10 18:25:24 -0300

    feat: header encode

--- src/dns/header.rs
+++ src/dns/header.rs
@@ -1,7 +1,11 @@
-use crate::{Decode, reader::PacketReader};
+use crate::{
+    Decode, Encode,
+    reader::PacketReader,
+    writer::{PacketWriter, WriteResult},
+};
 use std::io::Result;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct Header {
     id: u16,
     flags: u16,
@@ -33,5 +37,84 @@ impl Decode for Header {
             authority_count: u16::from_be_bytes([buf[8], buf[9]]),
             additional_count: u16::from_be_bytes([buf[10], buf[11]]),
         })
+    }
+}
+
+impl Encode for Header {
+    fn encode(&self, writer: &mut PacketWriter) -> WriteResult {
+        let mut n = 0;
+        n += writer.write_u16(self.id)?;
+        n += writer.write_u16(self.flags)?;
+        n += writer.write_u16(self.question_count)?;
+        n += writer.write_u16(self.answer_count)?;
+        n += writer.write_u16(self.authority_count)?;
+        n += writer.write_u16(self.additional_count)?;
+        Ok(n)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{Decode, Encode, dns::header::Header, reader::PacketReader, writer::PacketWriter};
+
+    #[test]
+    fn header_encode_has_12_bytes() {
+        let header = Header {
+            id: 0x1234,
+            flags: 0x8180,
+            question_count: 1,
+            answer_count: 2,
+            authority_count: 3,
+            additional_count: 4,
+        };
+
+        let mut writer = PacketWriter::new();
+
+        header.encode(&mut writer).unwrap();
+
+        assert_eq!(writer.get().len(), 12);
+    }
+    #[test]
+    fn header_encode_writes_correct_bytes() {
+        let header = Header {
+            id: 0x1234,
+            flags: 0x8180,
+            question_count: 1,
+            answer_count: 2,
+            authority_count: 3,
+            additional_count: 4,
+        };
+
+        let mut writer = PacketWriter::new();
+
+        header.encode(&mut writer).unwrap();
+
+        assert_eq!(
+            writer.get(),
+            &[
+                0x12, 0x34, 0x81, 0x80, 0x00, 0x01, 0x00, 0x02, 0x00, 0x03, 0x00, 0x04,
+            ]
+        );
+    }
+    #[test]
+    fn header_round_trip() {
+        let original = Header {
+            id: 0xABCD,
+            flags: 0x8180,
+            question_count: 1,
+            answer_count: 5,
+            authority_count: 2,
+            additional_count: 9,
+        };
+
+        let mut writer = PacketWriter::new();
+
+        original.encode(&mut writer).unwrap();
+
+        let mut reader = PacketReader::new(writer.get());
+
+        let decoded = Header::decode(&mut reader).unwrap();
+
+        assert_eq!(decoded, original);
     }
 }
