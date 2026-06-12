a2a0aa8352d08f93ef9c71b40745de594a42d39f
Leonardo C. <leonardorudolfocardoso@gmail.com>
2026-06-12 01:12:34 -0300

    feat: flags new type

--- src/dns/flags.rs
+++ src/dns/flags.rs
@@ -7,6 +7,78 @@ use crate::{
 #[derive(Debug, PartialEq, Eq, Copy, Clone)]
 pub struct Flags(u16);
 
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+pub enum QueryOrResponse {
+    Query,
+    Response,
+}
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+pub enum Opcode {
+    Query,
+    IQuery,
+    Status,
+    Unknown(u8),
+}
+
+#[derive(Debug)]
+pub enum ResponseCode {
+    NoError,
+    FormatError,
+    ServerFailure,
+    NameError,
+    NotImplemented,
+    Refused,
+    Unknown(u8),
+}
+
+impl Flags {
+    pub fn query_or_response(&self) -> QueryOrResponse {
+        if self.0 & (1 << 15) == 0 {
+            QueryOrResponse::Query
+        } else {
+            QueryOrResponse::Response
+        }
+    }
+
+    pub fn opcode(&self) -> Opcode {
+        match ((self.0 >> 11) & 0b1111) as u8 {
+            0 => Opcode::Query,
+            1 => Opcode::IQuery,
+            2 => Opcode::Status,
+            u => Opcode::Unknown(u),
+        }
+    }
+
+    pub fn authoritative_answer(&self) -> bool {
+        self.0 & (1 << 10) != 0
+    }
+
+    pub fn truncation(&self) -> bool {
+        self.0 & (1 << 9) != 0
+    }
+
+    pub fn recursion_desired(&self) -> bool {
+        self.0 & (1 << 8) != 0
+    }
+
+    pub fn recursion_available(&self) -> bool {
+        self.0 & (1 << 7) != 0
+    }
+
+    pub fn response_code(&self) -> ResponseCode {
+        match (self.0 & 0b1111) as u8 {
+            0 => ResponseCode::NoError,
+            1 => ResponseCode::FormatError,
+            2 => ResponseCode::ServerFailure,
+            3 => ResponseCode::NameError,
+            4 => ResponseCode::NotImplemented,
+            5 => ResponseCode::Refused,
+            other => ResponseCode::Unknown(other),
+        }
+    }
+}
+
 impl Decode for Flags {
     fn decode(reader: &mut PacketReader) -> std::io::Result<Self> {
         reader.read_u16().map(Flags)
